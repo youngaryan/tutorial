0efab792ff7406da1b477bb21cb4f546b4918ce1
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-07-24 23:11:23 +0100

    practice th:each

--- spring-boot-bootcamp-resources-main/02. model-view-controller/workbook 2.10/starter/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
+++ spring-boot-bootcamp-resources-main/02. model-view-controller/workbook 2.10/starter/workbook/src/main/java/com/ltp/workbook/WorkbookController.java
@@ -14,6 +14,18 @@ public class WorkbookController {
     
     @GetMapping(value="/")
     public String getStaff(Model model) {
+        
+        List<Employee> employees = Arrays.asList(
+            new Employee("Jim Halpert", 32, "Salesman"),
+            new Employee("Andy Bernard", 38, "Salesman"),
+            new Employee("Pam Beesly", 32, "Receptionist"),
+            new Employee("Michael Scott", 49, "Regional Manager"),
+            new Employee("Ryan Howard", 28, "Temp"),
+            new Employee("Angela Martin", 35, "Accountant"),
+            new Employee("Dwight Schrute", 37, "Assistant to the Regional Manager")
+        );
+
+        model.addAttribute("staff", employees);
         return "staff";
     }
     
