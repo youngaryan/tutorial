9c2377b541c50744a5d4bec7de5242584d1df852
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-07-24 23:03:32 +0100

    d

--- spring-boot-bootcamp-resources-main/02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeControler.java
+++ spring-boot-bootcamp-resources-main/02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeControler.java
@@ -1,5 +1,10 @@
 package com.ltp.gradesubmission;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +12,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class GradeControler {
 
+    List<Grades> studentGrades = Arrays.asList(new Grades("Harry", "Math", "C-"),
+     new Grades("Alex", "Physics", "B-"),
+      new Grades("Josef", "Potions", "A+"));
+
     @GetMapping("/grades")
     public String getGrades(Model model){
-        Grades grade = new Grades("Harry", "Math", "C-");
-        model.addAttribute("grade", grade);
+        model.addAttribute("grades", studentGrades);
         return "grades";
     }
 }
