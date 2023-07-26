a7ea11afc0de9909beb56d620a82783be72e263a
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-07-26 19:14:44 +0100

    good practice

--- spring-boot-bootcamp-resources-main/02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeControler.java
+++ spring-boot-bootcamp-resources-main/02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/GradeControler.java
@@ -1,24 +1,48 @@
 package com.ltp.gradesubmission;
 
-import java.lang.reflect.Array;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GradeControler {
 
-    List<Grades> studentGrades = Arrays.asList(new Grades("Harry", "Math", "C-"),
-     new Grades("Alex", "Physics", "B-"),
-      new Grades("Josef", "Potions", "A+"));
+    List<Grades> studentGrades = new ArrayList<>();
 
     @GetMapping("/grades")
     public String getGrades(Model model){
         model.addAttribute("grades", studentGrades);
         return "grades";
+    }
+
+    @GetMapping("/")
+    public String gradeForm(Model model, @RequestParam(required = false) String id){
+        int index = getGradeIndex(id);
+        model.addAttribute("gradeF", index == Constants.NOT_FOUND ?new Grades(): studentGrades.get(index));
+        return "form";
+    }
+
+    @PostMapping("/handleSubmmit")
+    public String submitGrade(Grades grades){
+        int index = getGradeIndex(grades.getId());
+        if(index == Constants.NOT_FOUND){
+            studentGrades.add(grades);
+        }else{
+            studentGrades.set(index, grades);
+        }
+        return "redirect:/grades";
+    }
+
+    public int getGradeIndex(String id){
+        for (int i = 0; i < studentGrades.size(); i++) {
+            if(studentGrades.get(i).getId().equals(id)) return i;
+        }
+        return Constants.NOT_FOUND;
     }
 }
--- spring-boot-bootcamp-resources-main/02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/Grades.java
+++ spring-boot-bootcamp-resources-main/02. model-view-controller/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/Grades.java
@@ -1,18 +1,23 @@
 package com.ltp.gradesubmission;
 
+import java.util.UUID;
+
 public class Grades {
     private String name;
     private String subject;
     private String score;
+    private String id;
 
-    
-    public Grades() {
-    }
-
-    public Grades(String name, String subject, String score) {
+    public Grades(String name, String subject, String score, String id) {
         this.name = name;
         this.subject = subject;
         this.score = score;
+        this.id = id;
+    }
+
+    
+    public Grades() {
+        this.id = UUID.randomUUID().toString();
     }
 
     public String getName() {
@@ -38,4 +43,22 @@ public class Grades {
     public void setScore(String score) {
         this.score = score;
     }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " name='" + getName() + "'" +
+            ", subject='" + getSubject() + "'" +
+            ", score='" + getScore() + "'" +
+            "}";
+    }
+
 }
