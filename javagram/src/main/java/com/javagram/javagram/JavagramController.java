6298f2363a5a30bedf8d63955c28d3b17930d381
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-07-28 14:46:16 +0100

    good practice with field validation

---
+++ javagram/src/main/java/com/javagram/javagram/JavagramController.java
@@ -0,0 +1,32 @@
+package com.javagram.javagram;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import jakarta.validation.Valid;
+
+@Controller
+public class JavagramController {
+
+    @GetMapping("/")
+    public String getForm(Model model){
+        model.addAttribute("user", new User());
+        return "sign-up";
+    }
+
+    @PostMapping("/submitItem")
+        public String handleForm(@Valid User user, BindingResult result){
+            if(result.hasErrors() == true) return "/sign-up";
+
+            return "redirect:/result";
+        }
+
+    @GetMapping("/result")
+    public String getResult() {
+        return "result";
+    }
+
+}
---
+++ javagram/src/main/java/com/javagram/javagram/User.java
@@ -0,0 +1,83 @@
+package com.javagram.javagram;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
+public class User {
+    @NotBlank(message = "First name must not be blank.")
+    @Size(min = 3, message = "First Name Must be Longer than 3 charachters.")
+    private String firstName;
+
+    @NotBlank(message = "Last name must not be blank.")
+    @Size(min = 3, message = "Last Name Must be Longer than 3 charachters.")
+    private String lastName;
+
+    @NotBlank(message = "Username must not be blank.")
+    @Size(min = 6, message = "Username Must be Longer than 6 charachters.")
+    private String userName;
+
+    @Email(message = "Email is not in a correct form.")
+    private String email;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @Past
+    private Date dateOfBirth;
+
+    public User(String firstName, String lastName, String userName, String email, Date dateOfBirth) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public User() {
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+    
+}
