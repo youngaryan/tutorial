b1b7e38da41fe8805bd25e2d91ba527e503e13de
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-07-31 22:33:52 +0100

    deleting unused imports

--- spring-boot-bootcamp-resources-main/04. three-layer-codebase/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/gradeController/GradeController.java
+++ spring-boot-bootcamp-resources-main/04. three-layer-codebase/grade-submission/starter/grade-submission/src/main/java/com/ltp/gradesubmission/gradeController/GradeController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.ltp.gradesubmission.Constants;
 import com.ltp.gradesubmission.Grade;
 import com.ltp.gradesubmission.gradeService.GradeService;
 
