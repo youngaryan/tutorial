3ab96eca8a06647848e1d991a196969a58853661
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-08-04 16:26:00 +0100

    rest api Get/Post/Delete/Put

--- spring-boot-bootcamp-resources-main/08. rest-api/contacts/starter/contacts/src/main/java/com/ltp/contacts/pojo/Contact.java
+++ spring-boot-bootcamp-resources-main/08. rest-api/contacts/starter/contacts/src/main/java/com/ltp/contacts/pojo/Contact.java
@@ -9,12 +9,6 @@ public class Contact {
     private String phoneNumber;
 
 
-    public Contact(String id, String name, String phoneNumber) {
-        this.id = id;
-        this.name = name;
-        this.phoneNumber = phoneNumber;
-    }
-
     public Contact() {
         this.id = UUID.randomUUID().toString();
     }
--- spring-boot-bootcamp-resources-main/08. rest-api/contacts/starter/contacts/src/main/java/com/ltp/contacts/service/ContactServiceImpl.java
+++ spring-boot-bootcamp-resources-main/08. rest-api/contacts/starter/contacts/src/main/java/com/ltp/contacts/service/ContactServiceImpl.java
@@ -1,10 +1,12 @@
 package com.ltp.contacts.service;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ltp.contacts.pojo.Contact;
 import com.ltp.contacts.repository.ContactRepository;
 
 @Service
@@ -13,8 +15,9 @@ public class ContactServiceImpl implements ContactService {
     @Autowired
     private ContactRepository contactRepository;
     
-
-
+    public Contact getContactById(String id){
+        return  contactRepository.getContact(findIndexById(id));
+    }
 
     private int findIndexById(String id) {
         return IntStream.range(0, contactRepository.getContacts().size())
@@ -23,4 +26,23 @@ public class ContactServiceImpl implements ContactService {
             .orElseThrow();
     }
 
+    @Override
+    public void saveContact(Contact contact) {
+        contactRepository.saveContact(contact);
+    }
+
+    @Override
+    public void updateContact(String id, Contact contact) {
+        contactRepository.updateContact(findIndexById(id), contact);
+    }
+
+    @Override
+    public void deleteContact(String id) {
+        contactRepository.deleteContact(findIndexById(id));
+    }
+
+    @Override
+    public List<Contact> getContacts() {
+        return contactRepository.getContacts();
+    }
 }
