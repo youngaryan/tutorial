312e10bba729c9dc57cdcb24f4727b1f1e966137
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-08-08 20:11:44 +0100

    couldn't do it as i wasnt able to override the "handleMethodArgumentNotValid"

--- spring-boot-bootcamp-resources-main/08. rest-api/workbook 8.3/starter/contacts/src/main/java/com/ltp/contacts/ApplicationExceptionHandler.java
+++ spring-boot-bootcamp-resources-main/08. rest-api/workbook 8.3/starter/contacts/src/main/java/com/ltp/contacts/ApplicationExceptionHandler.java
@@ -4,15 +4,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.ltp.contacts.exception.ContactNotFoundException;
 import com.ltp.contacts.exception.ErrorResponse;
 
 @ControllerAdvice
-public class ApplicationExceptionHandler {
-    @ExceptionHandler(ContactNotFoundException.class)
-    public ResponseEntity<Object> handleContactNotFoundException(ContactNotFoundException ex) {
-      ErrorResponse error = new ErrorResponse(ex.getLocalizedMessage());  
-      return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
-    }
+public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler {
+
+
+  @ExceptionHandler(ContactNotFoundException.class)
+  public ResponseEntity<Object> handleContactNotFoundException(ContactNotFoundException ex) {
+    ErrorResponse error = new ErrorResponse(ex.getLocalizedMessage());  
+    return new ResponseEntity<>(error, HttpStatus.NOT_FOUND);
+  }
+  
 }
+
+    
