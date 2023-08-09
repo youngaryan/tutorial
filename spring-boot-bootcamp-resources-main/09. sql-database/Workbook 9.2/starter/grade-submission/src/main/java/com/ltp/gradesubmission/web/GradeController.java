d77aee13ccd55c516260028bd18ef1ea99af448d
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-08-09 13:46:43 +0100

    more handeling controller

--- spring-boot-bootcamp-resources-main/09. sql-database/Workbook 9.2/starter/grade-submission/src/main/java/com/ltp/gradesubmission/web/GradeController.java
+++ spring-boot-bootcamp-resources-main/09. sql-database/Workbook 9.2/starter/grade-submission/src/main/java/com/ltp/gradesubmission/web/GradeController.java
@@ -1,12 +1,48 @@
 package com.ltp.gradesubmission.web;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ltp.gradesubmission.entity.Grade;
 
 @RestController
 @RequestMapping("/grade")
 public class GradeController {
-    
 
+    @GetMapping("/student/{studentId}/course/{courseId}")
+    public ResponseEntity<Grade> getGrade(@PathVariable long studentId, @PathVariable long courseId) {
+        return new ResponseEntity<Grade>(HttpStatus.OK);
+    }
+
+    @PostMapping("/student/{studentId}/course/{courseId}")
+    public ResponseEntity<Grade> saveGrade(@PathVariable long studentId, @PathVariable long courseId,
+            @RequestBody Grade grade) {
+        return new ResponseEntity<Grade>(grade, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/student/{studentId}/course/{courseId}")
+    public ResponseEntity<Grade> updateGrade(@PathVariable long studentId, @PathVariable long courseId,
+            @RequestBody Grade grade) {
+        return new ResponseEntity<Grade>(grade, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/student/{studentId}/course/{courseId}")
+    public ResponseEntity<HttpStatus> deleteGrade(@PathVariable long studentId, @PathVariable long courseId) {
+        return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/student/{studentId}")
+    public ResponseEntity<List<Grade>> getStudentGrades(@PathVariable long studentId) {
+        return new ResponseEntity<List<Grade>>(HttpStatus.OK);
+    }
 }
