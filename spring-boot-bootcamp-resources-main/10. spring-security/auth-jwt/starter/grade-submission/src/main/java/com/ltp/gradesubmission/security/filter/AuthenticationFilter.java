024f614876ee6c816cce5b64002d14db0fe1e992
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-08-21 14:21:33 +0100

    security, i have problem with vscode cant override

--- spring-boot-bootcamp-resources-main/10. spring-security/auth-jwt/starter/grade-submission/src/main/java/com/ltp/gradesubmission/security/filter/AuthenticationFilter.java
+++ spring-boot-bootcamp-resources-main/10. spring-security/auth-jwt/starter/grade-submission/src/main/java/com/ltp/gradesubmission/security/filter/AuthenticationFilter.java
@@ -1,5 +1,17 @@
 package com.ltp.gradesubmission.security.filter;
 
-public class AuthenticationFilter {
-    
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ltp.gradesubmission.entity.User;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
 }
