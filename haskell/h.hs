c96b4d5cf0b7d1f2451aaeceb717379396923951
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-10-03 14:40:15 +0100

    9

--- haskell/h.hs
+++ haskell/h.hs
@@ -1,17 +1,70 @@
+--import Data.Char (toUpper)
 main :: IO ()
-main = putStrLn (show (safeTail ("the trickier test string")))
+main = putStrLn (show (roots test))
+
+--roots 
+test = (1,-12,36)
+roots :: (Float, Float, Float) -> (Float, Float)
+roots (a, b, c)= 
+    if disc >= 0
+        then ((-b + sqrt disc) / (2*a), (-b - sqrt disc) / (2*a))
+        else (-10, -10)
+    where 
+        disc = b^2 - 4*a*c
 
 
-safeTail :: [a] -> [a]
-safeTail[] = []
-safeTail (x:xs) = xs
+--capitalizeStart 
+
+-- capitalizeStart :: String -> String
+-- capitalizeStart [] = [] 
+-- capitalizeStart (x:xs) = toUpper x : xs
+-- test = "hello hjm"
+
+--halves 
+-- halves :: [Int] -> [Int]
+-- halves[] = []
+-- halves (x:xs) = x `div` 2 : halves xs
+
+-- test = [2,4,6,8,10]
+
+--addc 
+-- addc :: Char -> String -> String
+-- addc c s = [c] ++ s
+
+
+-- --range 
+-- range :: Int -> Bool
+-- range x = if x <= 20 && x>=1 then
+--     True
+--     else
+--     False
+
+
+
+
+--test :: [Int]
+
+
+--stack
+-- test = [1,2,3,4,5]
+-- stack :: a ->[a] -> [a]
+-- stack h [] = [h]
+-- stack h (x:xs) = xs ++ [h]
+
+ --stack takes the first element of a list and puts it on the end of a list
+
+--[1,2,3,4,5]
+--[2,3,4,5,1]
+
+-- safeTail :: [a] -> [a]
+-- safeTail[] = []
+-- safeTail (x:xs) = xs
 -- safeTail x = if null x then
 --         []
 --     else
 --         tail x
 -- safeTail a = tail a
 -- safeTail [] = []
-
 
 -- firstWord :: String -> String
 
@@ -20,11 +73,6 @@ safeTail (x:xs) = xs
 -- eq :: Floating a => (a, a, a, a) -> a
 
 -- eq (x1,y1,x2,y2) = (sqrt((x2 - x1)^2 + (y2 - y1)^2))
-
-
-
-
-
 
 
 
@@ -55,4 +103,4 @@ safeTail (x:xs) = xs
 -- N = a 'div' length xs
 -- where
 -- a = 10
--- xs = [1, 2, 3, 4, 5
+-- xs = [1, 2, 3, 4, 5]
