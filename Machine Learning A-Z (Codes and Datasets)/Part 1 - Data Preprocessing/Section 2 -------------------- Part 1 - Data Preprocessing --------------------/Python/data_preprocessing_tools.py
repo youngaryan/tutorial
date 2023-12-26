e27bd605d32cc4afcd3173f6fcfe6ded0a4faf94
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-12-26 22:35:35 +0000

    1

--- Machine Learning A-Z (Codes and Datasets)/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Python/data_preprocessing_tools.py
+++ Machine Learning A-Z (Codes and Datasets)/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Python/data_preprocessing_tools.py
@@ -15,11 +15,11 @@ print("---------------------")
 
 # Taking care of missing data
 from sklearn.impute import SimpleImputer
-imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer = SimpleImputer(missing_values = np.nan , strategy = 'mean')
 imputer.fit(X[:, 1:3])
-X[:, 1:3] = imputer.transform(X[:, 1:3])
+X[:,1:3]=imputer.transform(X[:, 1:3])
 print(X)
-
+print("---------------------")
 # Encoding categorical data
 # Encoding the Independent Variable
 from sklearn.compose import ColumnTransformer
@@ -27,19 +27,16 @@ from sklearn.preprocessing import OneHotEncoder
 ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
 X = np.array(ct.fit_transform(X))
 print(X)
+print("---------------------")
 # Encoding the Dependent Variable
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
-y = le.fit_transform(y)
+y = le.fit_transform(y=y)
 print(y)
-
 # Splitting the dataset into the Training set and Test set
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
-print(X_train)
-print(X_test)
-print(y_train)
-print(y_test)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
