0300a200b6eedf9fa42d690b1d601a20f0e6f193
Aryan Golbaghi <aryangolbaghi@gmail.com>
2023-12-26 00:31:53 +0000

    1

--- Machine Learning A-Z (Codes and Datasets)/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Python/data_preprocessing_tools.py
+++ Machine Learning A-Z (Codes and Datasets)/Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Python/data_preprocessing_tools.py
@@ -8,9 +8,10 @@ import pandas as pd
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
+y = dataset.iloc[:,-1].values
 print(X)
 print(y)
+print("---------------------")
 
 # Taking care of missing data
 from sklearn.impute import SimpleImputer
